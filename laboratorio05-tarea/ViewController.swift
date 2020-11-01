33d9cd01d54cd94be593cfa69fc8a6aa18761c80
Marcocsx2 <marco.vizcarra@tecsup.edu.pe>
2020-11-01 15:21:39 -0800

    laboratorio05

---
+++ laboratorio05-tarea/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  laboratorio05-tarea
+//
+//  Created by Marco Vizcarra on 11/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
